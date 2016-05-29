1439ceceb52418b145a3c85bd5616ebf59f64253
zhou Mr <uubs@yeah.net>
2016-05-29 09:20:32 +0900

    Initial Commit

---
+++ BlurEffectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurEffectTest
+//
+//  Created by Mrzhou on 16/5/29.
+//  Copyright © 2016年 Mrzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
